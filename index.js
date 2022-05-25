6c166dbb8bf430b6e5a17b2552e38c39f091fe18
owakeelAhmmed <mdowakeelahmed5678@gmail.com>
2022-05-25 22:04:22 +0600

    server connected to mongodb

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const express = require('express')
-const  cors  = require('cors')
-require ('dotenv').config();
-const app = express()
-const port =process.env.PORT || 5000
-
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello super-gear!')
-})
 
-app.listen(port, () => {
-  console.log(`super-gear app listening on port ${port}`)
-})
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.urclyui.mongodb.net/?retryWrites=true&w=majority`;
+
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+    async function run(){
+
+      try{
+        await client.connect();
+        console.log("connected to database")
+      }
+      finally{
+
+      }
+
+
+
+    }
+    run().catch(console.dir);
+
+
+
+
+
+
+    app.get('/', (req, res) => {
+      res.send('Sepeu Gear Server Is Runing')
+    });
+
+    app.listen(port, () => {
+      console.log(`SuperGear App listening on port ${port}`)
+    })
