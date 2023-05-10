dfaa0a98016426af1f71fe3f7717702f36d9f284
Md Owakeel Ahmmed <96741142+owakeelAhmmed@users.noreply.github.com>
2023-05-11 02:25:02 +0600

    new server create and connect

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
       
 
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.urclyui.mongodb.net/?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vl8ctt6.mongodb.net/`;
 
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
